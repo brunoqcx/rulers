2b35c6228ee17ba5c99d8aabe5e983aba63e52d4
BrunoQcx <brunotavm@gmail.com>
2019-02-07 23:26:56 -0200

    Creating and updating

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,5 +1,6 @@
 require 'erubis'
 require_relative 'file_model'
+require 'rack/request'
 
 module Rulers
   class Controller
@@ -16,6 +17,20 @@ module Rulers
       eruby = Erubis::Eruby.new(template)
       eruby.result locals.merge(instance_variable_hash)
     end
+
+    def response(text, status = 200, headers = {})
+      raise 'Already responded!' if @response
+      a = [text].flatten
+      @response =Rack::Response.new(a, status, headers)
+    end
+
+    def get_response
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
     
     def controller_name
       klass = self.class
@@ -29,6 +44,14 @@ module Rulers
 
     def instance_variable_hash
       instance_variables.each_with_object({}) { |k, v| v[k] = instance_variable_get(k) }
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
     end
   end
 end
--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -1,8 +1,11 @@
 require "multi_json"
+require 'json'
 
 module Rulers
   module Model
     class FileModel
+      attr_reader :hash
+
       def initialize(filename)
         @filename = filename
         basename = File.split(filename)[-1]
@@ -32,6 +35,28 @@ module Rulers
       def self.all
         files = Dir['db/quotes/*.json']
         files.map { |file|  FileModel.new file }
+      end
+
+      def self.create(attrs)
+        hash = attrs.slice("submitter", "quote", "attribution")
+
+        files = Dir['db/quotes/*.json']
+        names = files.map { |f| File.split(f)[-1] }
+        highest = names.map { |b| b.to_i }.max
+        new_id = highest + 1
+
+        File.write("db/quotes/#{new_id}.json", hash.to_json)
+
+        FileModel.new("db/quotes/#{new_id}.json")
+      end
+
+      def self.update(id)
+        model = FileModel.find(id)
+        model['submitter'] = 'outro_cara'
+
+        File.write("db/quotes/#{id}.json", model.hash.to_json)
+        
+        FileModel.new("db/quotes/#{id}.json")
       end
     end
   end
